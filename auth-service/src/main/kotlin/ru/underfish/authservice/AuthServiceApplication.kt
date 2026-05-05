f49bb1a0aa6ed34c36e934a3882dba436f55bf8b
alex <a9@gg.ru>
2026-05-05 15:31:04 +0300

    adapt to new API

--- auth-service/src/main/kotlin/ru/underfish/authservice/AuthServiceApplication.kt
+++ auth-service/src/main/kotlin/ru/underfish/authservice/AuthServiceApplication.kt
@@ -2,10 +2,13 @@ package ru.underfish.authservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.cloud.openfeign.EnableFeignClients
+import ru.underfish.authservice.client.config.ProfileClientProperties
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableConfigurationProperties(ProfileClientProperties::class)
 class AuthServiceApplication
 
 fun main(args: Array<String>) {
--- auth-service/src/main/kotlin/ru/underfish/authservice/client/ProfileClient.kt
+++ auth-service/src/main/kotlin/ru/underfish/authservice/client/ProfileClient.kt
@@ -1,18 +1,21 @@
 package ru.underfish.authservice.client
 
 import org.springframework.cloud.openfeign.FeignClient
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import ru.underfish.authservice.client.config.ProfileClientConfig
 import ru.underfish.authservice.client.dto.ProfileRegisterRequest
 import ru.underfish.authservice.dto.response.UserResponse
 
-// Wire this into AuthService.register() once profile-service exposes an internal endpoint
-// that accepts user data without a password (legacy monolith still requires it).
-@FeignClient(name = "profile-service", url = "\${services.profile.url}")
+@FeignClient(
+    name = "profile-service",
+    url = "\${services.profile.url}",
+    configuration = [ProfileClientConfig::class],
+)
 interface ProfileClient {
-    @PostMapping("/api/v1/profiles/register")
+    @PutMapping("/api/v1/profiles/register")
     fun registerProfile(
         @RequestBody request: ProfileRegisterRequest,
     ): UserResponse
