d740110a11cc4de223f8f765af98bcf6bc412a39
alex <a9@gg.ru>
2026-05-05 15:20:32 +0300

    fix register endpoint

--- profile-service/src/main/kotlin/ru/underfish/profile_service/controller/UserController.kt
+++ profile-service/src/main/kotlin/ru/underfish/profile_service/controller/UserController.kt
@@ -3,11 +3,10 @@ package ru.underfish.profile_service.controller
 
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
-import ru.underfish.profile_service.dto.request.UserLoginRequest
 import ru.underfish.profile_service.dto.request.UserRegistrationRequest
 import ru.underfish.profile_service.dto.request.UserUpdateRequest
-import ru.underfish.profile_service.dto.response.UserLoginResponse
 import ru.underfish.profile_service.dto.response.UserResponse
+import ru.underfish.profile_service.exception.ForbiddenException
 import ru.underfish.profile_service.security.CurrentUserProvider
 import ru.underfish.profile_service.service.UserService
 
@@ -17,7 +16,18 @@ class UserController(
     private val userService: UserService,
     private val currentUserProvider: CurrentUserProvider,
 ) {
-    
+
+    @PutMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    fun registerUserPut(
+        @RequestBody request: UserRegistrationRequest,
+    ): UserResponse {
+        if (!currentUserProvider.isAdmin()) {
+            throw ForbiddenException("Access denied")
+        }
+        return userService.registerUser(request)
+    }
+
     @GetMapping("/{user_id}")
     fun getUserById(
         @PathVariable("user_id") userId: Long
@@ -43,9 +53,8 @@ class UserController(
         @PathVariable("user_id") userId: Long,
     ) {
         if (!currentUserProvider.isAdmin()) {
-            throw RuntimeException("Forbidden")
+            throw ForbiddenException("Access denied")
         }
         userService.deleteUser(userId)
     }
 }
-
