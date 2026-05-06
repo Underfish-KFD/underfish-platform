8aeb93b25813a9ad5f37fa4323ac6d423484ad4b
Trufanov Aleksandr <83272960+drlalka@users.noreply.github.com>
2026-05-07 02:42:23 +0300

    Add file route, bump jackson test dep, include organizerId in event response

--- integration-tests/build.gradle.kts
+++ integration-tests/build.gradle.kts
@@ -9,7 +9,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
     testImplementation("com.nimbusds:nimbus-jose-jwt:9.37")
-    testImplementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.4")
+    testImplementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.2")
     testImplementation("org.postgresql:postgresql:42.6.0")
 }
 
